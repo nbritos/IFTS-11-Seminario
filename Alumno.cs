aa9e9494d976bcdcf0748de4470184670e09086f
Nicolas <nbritos1988@gmail.com>
2021-08-18 18:59:52 -0300

    menu completo, clase alumno completo, faltan las excep

---
+++ Alumno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeminarioUnidad1
+{
+    class Alumno
+    {
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNacimiento;
+        private int documento;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public int Documento { get => documento; set => documento = value; }
+
+
+        public Alumno(string nombre, string ape, DateTime fechanac,int dni)
+        {
+            this.Nombre = nombre;
+            this.apellido = ape;
+            this.fechaNacimiento = fechanac;
+            this.documento = dni;
+        }
+
+        public int Edad(DateTime nac)
+        {
+            return DateTime.Now.Year - nac.Year;
+        }
+
+        public override String ToString()
+        {
+            return ($"Alumno: {this.Nombre}\nApellido: {this.apellido.ToString()}\nDocumento: {this.documento.ToString()}\nEdad: {this.Edad(this.fechaNacimiento).ToString()}");
+        }
+    }
+}
---
+++ Ejecutora.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeminarioUnidad1
+{
+    class Ejecutora
+    {
+
+        static void Main(string[] args)
+        {
+            List<Alumno> listaalumno = new List<Alumno>();
+
+            int salir=0;
+            string var1, var2;
+            DateTime var3 = new DateTime();
+            int var4, menu = 1;
+
+
+            while (menu!=0)
+            {
+                Console.WriteLine("1. Cargar alumno\t2. Eliminar segun DNI\t3. Mostrar todos los alumnos de la lista");
+                switch (menu)
+                {
+                    case 1:
+                        do
+                        {
+                            Console.WriteLine("Ingrese nombre del alumno ");
+                            var1 = Console.ReadLine();
+                            Console.WriteLine("Ingrese nombre del alumno ");
+                            var2 = Console.ReadLine();
+                            Console.WriteLine("Ingrese fecha de nacimiento (format...) ");
+                            try
+                            {
+                                var3 = Convert.ToDateTime(Console.ReadLine());
+                            }
+                            catch (FormatException f1)
+                            {
+                                Console.WriteLine(f1.StackTrace);
+                            }
+
+                            Console.WriteLine("Ingrese DNI del alumno ");
+                            var4 = int.Parse(Console.ReadLine());
+                            listaalumno.Add(new Alumno(var1, var2, var3, var4));
+
+                            Console.WriteLine("salir?");
+                            salir = int.Parse(Console.ReadLine());
+
+                        } while (salir != 0);
+                        break;
+
+                    case 2:
+                        int alumDocumento;
+                        Console.WriteLine("Ingrese apellido a remover");
+                        alumDocumento = int.Parse(Console.ReadLine());
+
+                        Alumno aRemover = listaalumno.Find(x => x.Documento == alumDocumento);
+                        if (aRemover != null) listaalumno.Remove(aRemover);
+                        break;
+
+                    case 3:
+                        foreach (Alumno alum in listaalumno)
+                        {
+                            Console.WriteLine(alum.ToString() + "\n");
+                        }
+                        break;
+
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Esto es to-to-todos");
+                        Console.ResetColor();
+                        break;
+
+                    default:
+                        Console.WriteLine("opcion incorrecta");
+                        break;
+                }
+            } 
+        }
+    }
+}
